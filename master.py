41c84e1483f9e815b1f92107a99b854693ef3670
tazz4843 <dustinwwilk@rvschools.ab.ca>
2018-05-14 16:18:16 -0600

    Update master.py

--- master.py
+++ master.py
@@ -1,5 +1,8 @@
 import RPi.GPIO as GPIO
 
-version = "0.0.0 beta"
+version = "0.0.0 alpha"
 
 print ("Welcome to Helicopter! Version " + version)
+
+if (version == "0.0.0 alpha")
+    print ("This is an early alpha. The helicopter may be unstable.")
